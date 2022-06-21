3e5e1f1b70a4c5702d37c0fbc36dd59d49a073ba
JMchfundi <JMchfundi@gmail.com>
2022-06-21 10:35:02 +0000

    ce

--- fragments/header.php
+++ fragments/header.php
@@ -57,7 +57,7 @@
           <li><a href="index.php" class="active">Home</a></li>
           <li><a href="about.php">About</a></li>
           <li><a href="services.php">Services</a></li>
-          <li><a href="#">Portfolio</a></li>
+          <li><a href="portfolio.php">Portfolio</a></li>
           <!-- <li><a href="team.php">Team</a></li> -->
           <!-- <li><a href="blog.html">Blog</a></li>
           <li class="dropdown"><a href="#"><span>Dropdown</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
