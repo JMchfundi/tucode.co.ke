042d7662266cc87bebb214c43c96b00ded16646f
Jafar Mchasa Akida <JMchfundi@gmail.com>
2022-06-30 14:58:39 +0300

    ce

--- contact.php
+++ contact.php
@@ -55,13 +55,8 @@
             </div>
 
             <div class="col-lg-6" data-aos="fade-up" data-aos-delay="250">
-            <?php include "mail.php";?>  
-                <!-- Error display -->
-            <div>
-                <p class="success"> <?php echo $success;  ?> </p>
-                <p class="failed"> <?php echo $failed;  ?> </p>
-            </div>
-              <form action="" method="post" role="form" class="php-email-form">
+                
+              <form action="mail.php" method="post" role="form" class="php-email-form">
                 <div class="row">
                   <div class="col-md-6 form-group">
                     <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" required>
@@ -74,7 +69,7 @@
                   <textarea class="form-control" name="message" rows="5" placeholder="Message" required></textarea>
                 </div>
 
-                <div class="text-center"><button type="submit" name="submit">Send Message</button></div>
+                <div class="text-center"><button type="submit">Send Message</button></div>
               </form>
 
             </div><!-- End Contact Form -->
