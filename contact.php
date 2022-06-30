1de4312b9049d4892e2b093317c90d3d3a40b4c9
Jafar Mchasa Akida <JMchfundi@gmail.com>
2022-06-30 12:58:10 +0300

    ce

--- contact.php
+++ contact.php
@@ -56,7 +56,7 @@
 
           <div class="col-lg-6" data-aos="fade-up" data-aos-delay="250">
 
-            <form action="mail.php" method="post" role="form" class="php-email-form">
+            <!-- <form action="mail.php" method="post" role="form" class="php-email-form">
               <div class="row">
                 <div class="col-md-6 form-group">
                   <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" required>
@@ -70,7 +70,7 @@
               </div>
 
               <div class="text-center"><button type="submit">Send Message</button></div>
-            </form>
+            </form> -->
 
           </div><!-- End Contact Form -->
 
