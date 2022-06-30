0a525bdd846473c9be10d0a5834f2346f2315188
Jafar Mchasa Akida <JMchfundi@gmail.com>
2022-06-30 12:37:01 +0300

    ce

--- contact.php
+++ contact.php
@@ -56,7 +56,7 @@
 
           <div class="col-lg-6" data-aos="fade-up" data-aos-delay="250">
 
-            <form action="#" method="post" role="form" class="php-email-form">
+            <form action="forms/mail.php" method="post" role="form" class="php-email-form">
               <div class="row">
                 <div class="col-md-6 form-group">
                   <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" required>
@@ -66,10 +66,7 @@
                 </div>
               </div>
               <div class="form-group mt-3">
-                <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" required>
-              </div>
-              <div class="form-group mt-3">
-                <textarea class="form-control" name="message" rows="5" placeholder="Message" required></textarea>
+                <textarea class="form-control" name="comments" rows="5" placeholder="Message" required></textarea>
               </div>
 
               <div class="text-center"><button type="submit">Send Message</button></div>
