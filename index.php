15fb106d34467b771a5473ab300a2d21932322d2
Akida M Jafari <jmchfundi@gmail.com>
2022-07-07 14:19:52 +0300

    Update index.php

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
             </p>
           </blockquote>
           <div class="d-flex" data-aos="fade-up" data-aos-delay="200">
-            <a href="#about" class="btn-get-started">Get Started</a>
+            <a href="services.php" class="btn-get-started">Get Started</a>
             <a href="about-video.php" class="glightbox btn-watch-video d-flex align-items-center"><i class="bi bi-play-circle"></i><span>Watch Video</span></a>
           </div>
 
