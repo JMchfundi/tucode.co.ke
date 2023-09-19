885d97975abacb85b86d2bf078f55c718e1c64e9
Jafari Mchasa Akida - Co-founder & CEO, TuCode Tech Solutions <Jakida@tucode.co.ke>
2023-09-19 11:13:41 +0300

    runvue

--- src/components/HomeTTS.vue
+++ src/components/HomeTTS.vue
@@ -1,14 +1,5 @@
 <template>
 <div>
-    <!-- Spinner Start -->
-    <div id="spinner" class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
-        <div class="spinner-border text-primary" style="width: 3rem; height: 3rem;" role="status">
-            <span class="sr-only">Loading...</span>
-        </div>
-    </div>
-    <!-- Spinner End -->
-
-
     <!-- Topbar Start -->
     <div class="container-fluid bg-light d-none d-lg-block">
         <div class="row align-items-center top-bar">
