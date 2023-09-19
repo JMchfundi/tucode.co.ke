76f3350d84eed76b52564b13dee94e38b278dae5
Jafari Mchasa Akida - Co-founder & CEO, TuCode Tech Solutions <Jakida@tucode.co.ke>
2023-09-19 10:58:02 +0300

    few changes

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,36 @@
 <template>
-  <div></div>
+    <HeaderCodeVue></HeaderCodeVue>
+    <HomeLayer></HomeLayer>
+    <ServiceCodeVue></ServiceCodeVue>
+    <AboutCode></AboutCode>
+    <ServiceSlide></ServiceSlide>
+    <BookCode></BookCode>
+    <TeamCode></TeamCode>
+    <FooterCode></FooterCode>
 </template>
 
 <script>
 // @ is an alias to /src
+import HeaderCodeVue from '@/components/HeaderCode.vue';
+import ServiceCodeVue from '@/components/ServiceCode.vue';
+import AboutCode from '@/components/AboutCode.vue';
+import ServiceSlide from '@/components/ServiceSlide.vue';
+import BookCode from '@/components/BookCode.vue';
+import TeamCode from '@/components/TeamCode.vue';
+import FooterCode from '@/components/FooterCode.vue'
+import HomeLayer from '@/components/HomeLayer.vue';
 
 export default {
   name: 'HomeView',
   components: {
+    HeaderCodeVue,
+    ServiceCodeVue,
+    AboutCode,
+    ServiceSlide,
+    BookCode,
+    TeamCode,
+    FooterCode,
+    HomeLayer
   }
 }
 </script>
