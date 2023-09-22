af7572b6e625a64debbf0944ec559103e34a018e
Admin <omarimwakaribu92@gmail.com>
2023-09-22 14:44:49 +0300

    Another last Edition

--- src/components/TeamCode.vue
+++ src/components/TeamCode.vue
@@ -10,7 +10,7 @@
                 <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
                     <div class="team-item">
                         <div class="position-relative overflow-hidden">
-                            <img class="img-fluid" src="../../public/img/jafari_mchasa.jpeg" alt="">
+                            <img class="img-fluid" src="../../public/img/jafari_pic_01.jpeg" alt="">
                         </div>
                         <div class="team-text">
                             <div class="bg-light">
