eb1f4f54b29b165080f633d96203f16c046eefad
Jafari Mchasa Akida - Co-founder & CEO, TuCode Tech Solutions <Jakida@tucode.co.ke>
2023-09-22 11:57:52 +0300

    reload service

--- src/components/HeaderCode.vue
+++ src/components/HeaderCode.vue
@@ -41,8 +41,7 @@
                     <a href="" class="nav-item nav-link active">Home</a>
                     <router-link to="/about" class="nav-item nav-link">About</router-link>
                     <!-- <router-link to="/team" class="nav-item nav-link">Our Team</router-link> -->
-                    <a href="https://www.vue.tucode.co.ke/#/service" class="nav-item nav-link"
-                    onclick="window.location.reload(true)">Services</a>
+                    <router-link :to="{name: 'service'}" class="nav-item nav-link">Services</router-link>
                     <!-- <div class="nav-item dropdown">
                         <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
                         <div class="dropdown-menu fade-up m-0">
